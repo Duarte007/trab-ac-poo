4d5c769a7d51e67f1c468b364ff7f4cb6124637a
ianrcsd <54161291+ianrcsd@users.noreply.github.com>
2019-10-14 22:05:16 -0300

    Criação MENU

--- ULA/Program.cs
+++ ULA/Program.cs
@@ -10,6 +10,40 @@ namespace ULA {
             Console.WriteLine("Hello World!");
             Console.WriteLine(and.getBit1());
             Console.WriteLine(and.getBit2());
+
+            int numA, numB, operacao;
+            int[] A, B, op, resultado;
+            char rep;
+            do
+            {
+                Console.Clear();
+                do
+                {
+                    Console.Write("Insira um 0 ou 1 para A: ");
+                } while (!int.TryParse(Console.ReadLine(), out numA) && (numA <= 0 || numA >= 1));
+
+                do
+                {
+                    Console.Write("Insira 0 ou 1 para B: ");
+                } while (!int.TryParse(Console.ReadLine(), out numB) && (numA <= 0 || numA >= 1));
+
+                Console.Clear();
+                Console.WriteLine("A: " + numA.ToString() + " | B: " + numB.ToString());
+                Console.Write("\n\nA and B (0)\nA or B (1)\nA xor B (2)\nA' (3)\nB' (4)\nA + B" + "\n\nEscolha a operação: ");
+                while (!int.TryParse(Console.ReadLine(), out operacao) && (operacao < 0 || operacao > 4)) ;
+
+                Console.Clear();
+                Console.WriteLine("A: " + numA.ToString() + " | B: " + numB.ToString());
+               
+                do
+                {
+                    Console.Write("\n\nDeseja continuar? (s/n)");
+                } while (char.TryParse(Console.ReadLine().ToLower(), out rep) && (!rep.Equals('s') && !rep.Equals('n')));
+
+            } while (rep.Equals('s'));
+
+
         }
     }
-}
+    }
+
